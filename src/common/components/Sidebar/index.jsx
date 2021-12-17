2223980e4e2d70b731f0fc485b510ad8b92cd67e
Pedro Cruz <47664638+pedrovictorcruz@users.noreply.github.com>
2021-12-17 00:27:49 -0300

    First Commit

---
+++ src/common/components/Sidebar/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Sidebar extends Component {
+    render() {
+        return (
+          <div className="mx-6">
+            <div className="w-4">
+              <h1 className="font-bold">Teste</h1>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Sidebar;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Sidebar from '../../common/components/Sidebar';
+
+const Home = () => {
+  return (
+    <Sidebar/>
+  )
+}
+
+export default Home;
